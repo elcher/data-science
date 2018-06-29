d4a6228ecbdc9f2172fe49b3438c06980cb18222
elcher <emma.hwang.lc@gmail.com>
2018-06-28 23:51:54 -0500

    Nikola auto commit.
    Nikola version: 8.0.0b2

--- conf.py
+++ conf.py
@@ -92,7 +92,7 @@ DEFAULT_LANG = "en"
 # The format is {"translationcode" : "path/to/translation" }
 # the path will be used as a prefix for the generated pages location
 TRANSLATIONS = {
-    DEFAULT_LANG: "en",
+    DEFAULT_LANG: "",
     # Example for another language:
     # "es": "./es",
 }
